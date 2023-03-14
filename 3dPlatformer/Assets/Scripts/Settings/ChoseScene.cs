e92582c59c613994e111ed98b46711582d984c65
praprf <fil18mot@gmail.com>
2023-03-14 06:10:06 +0300

    Settings Scene done!

---
+++ 3dPlatformer/Assets/Scripts/Settings/ChoseScene.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ChoseScene : MonoBehaviour
+{
+    [SerializeField] private Button _buttonPlay;
+    [SerializeField] private Button _buttonChose;
+    [SerializeField] private Button _buttonSettings;
+    [SerializeField] private Button _buttonQuit;
+    public void ManageScene(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+    public void Quiting()
+    {
+        Application.Quit();
+    }
+
+}
---
+++ 3dPlatformer/Assets/Scripts/Settings/SettingsScript.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SettingsScript : MonoBehaviour
+{
+    [SerializeField] private TMPro.TMP_Dropdown _resolutionDropdown;
+    [SerializeField] private TMPro.TMP_Dropdown _qualityDropdown;
+
+    Resolution[] resolutions;
+    void Start()
+    {
+        _resolutionDropdown.ClearOptions();
+        List<string> options = new List<string>();
+        resolutions = Screen.resolutions;
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height + " " + resolutions[i].refreshRate + "Hz";
+            options.Add(option);
+            if (resolutions[i].width == Screen.currentResolution.width
+                  && resolutions[i].height == Screen.currentResolution.height)
+                currentResolutionIndex = i;
+        }
+
+        _resolutionDropdown.AddOptions(options);
+        _resolutionDropdown.RefreshShownValue();
+        LoadSettings(currentResolutionIndex);
+    }
+
+   
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width,
+                  resolution.height, Screen.fullScreen);
+    }
+
+
+    public void SetQuality(int qualityIndex)
+    {
+
+        QualitySettings.SetQualityLevel(qualityIndex);
+
+    }
+
+    public void ExitGame()
+    {
+        SceneManager.LoadScene("FirstScene");
+    }
+
+    public void SaveSettings()
+    {
+        PlayerPrefs.SetInt("QualitySettingPreference",
+                   _qualityDropdown.value);
+        PlayerPrefs.SetInt("ResolutionPreference",
+                   _resolutionDropdown.value);
+        PlayerPrefs.SetInt("FullscreenPreference",
+                   System.Convert.ToInt32(Screen.fullScreen));
+        
+    }
+
+    public void LoadSettings(int currentResolutionIndex)
+    {
+        if (PlayerPrefs.HasKey("QualitySettingPreference"))
+           _qualityDropdown.value =
+                         PlayerPrefs.GetInt("QualitySettingPreference");
+        else
+            _qualityDropdown.value = 3;
+        if (PlayerPrefs.HasKey("ResolutionPreference"))
+            _resolutionDropdown.value =
+                         PlayerPrefs.GetInt("ResolutionPreference");
+        else
+            _resolutionDropdown.value = currentResolutionIndex;
+        if (PlayerPrefs.HasKey("FullscreenPreference"))
+            Screen.fullScreen =
+            System.Convert.ToBoolean(PlayerPrefs.GetInt("FullscreenPreference"));
+        else
+            Screen.fullScreen = true;
+        
+          
+    }
+}
