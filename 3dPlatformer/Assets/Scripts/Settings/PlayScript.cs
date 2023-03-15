36443607b2ff60015c339bdab5d72c5f31126721
praprf <fil18mot@gmail.com>
2023-03-16 02:34:33 +0300

    add logic for button play in meny

---
+++ 3dPlatformer/Assets/Scripts/Settings/PlayScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayScript : MonoBehaviour
+{
+    [SerializeField] private Button _play;
+    public void Lvl1()
+    {
+        int lvl = PlayerPrefs.GetInt("levels");
+        if(lvl > 3)
+        {
+            SceneManager.LoadScene("Level3");
+        }
+        else
+        {
+            SceneManager.LoadScene("Level" + lvl.ToString());
+        }
+    }
+}
